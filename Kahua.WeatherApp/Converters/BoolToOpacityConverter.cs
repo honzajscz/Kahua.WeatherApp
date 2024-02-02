725a30d4dacdc092fd12ed184507cb7c3b5e7cca
Jan Vratislav <jan.vratislav@gmail.com>
2024-02-02 01:38:05 +0100

    Placing a type to a separate file

--- Kahua.WeatherApp/Converters/BoolToOpacityConverter.cs
+++ Kahua.WeatherApp/Converters/BoolToOpacityConverter.cs
@@ -3,17 +3,16 @@ using Microsoft.UI.Xaml.Data;
 
 namespace Kahua.WeatherApp.Converters;
 
-public class BoolNegationConverter : IValueConverter
+public class BoolToOpacityConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, string language)
     {
-        if (value is bool booleanValue) return !booleanValue;
+        if (value is bool booleanValue) return booleanValue ? 1 : 0;
         return value;
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, string language)
     {
-        if (value is bool booleanValue) return !booleanValue;
-        return value;
+        throw new NotImplementedException();
     }
 }
